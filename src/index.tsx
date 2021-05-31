0bd5dd2a10e91812de990e9fd78f1d7e9a70743d
Gustavo Costa <cmtehenz@gmail.com>
2021-05-31 15:20:38 -0300

    :construction: _Erro Context

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          category: 'Dev',
+          type: 'deposit',
+          amount: 6000,
+          createdAt: new Date('2021-02-06 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          category: 'Habitação',
+          type: 'withdraw',
+          amount: 1300,
+          createdAt: new Date('2021-02-10 09:00:00'),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = 'api';
 
-    this.get('/transactions', () => [
-      {
-        id: 1,
-        title: 'Transaction 1',
-        amount: 400,
-        type: 'deposit',
-        category: 'Food',
-        createdAt: new Date(),
-      },
-    ]);
+    this.get('/transactions', () => this.schema.all('transaction'));
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
+    });
   },
 });
 
